c81605cea4ca47835407744b69c20e5cbe6257ba
sA-Ge1 <adityakarumbaiah@gmail.com>
2024-10-11 15:59:48 +0530

    working

---
+++ build/robotic_arm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sage/arm/src/robotic_arm
+# Build directory: /home/sage/arm/build/robotic_arm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/your_moveit_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/sage/arm/install/robotic_arm/share/robotic_arm/package.sh"
